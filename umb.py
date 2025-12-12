98b65d7e226adacc489bd0fa8f432b09aa8ed916
Eldan445 <elishadaniel911@email@gmail.com>
2025-12-12 15:37:39 +0100

    Force Python 3.11 and fix cloud dependencies

--- umb.py
+++ umb.py
@@ -1,3 +1,9 @@
+import sys
+try:
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
 import chromadb
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction 
 import uuid
